dca907c8962424d01fe77e985a84a246df18639a
pvlach <vlach1989@gmail.com>
2020-03-16 11:08:21 +0100

    Updates

---
+++ precache-manifest.0bd941626a2c7d363eeafaef33ff037a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd0fef95f3ab2d1f161902c70cc7aaf",
+    "url": "/app-docs/index.html"
+  },
+  {
+    "revision": "45aaa542e4acf24e7bc1",
+    "url": "/app-docs/static/css/2.fcbb9a24.chunk.css"
+  },
+  {
+    "revision": "858a7c063c8986853998",
+    "url": "/app-docs/static/css/main.2127a503.chunk.css"
+  },
+  {
+    "revision": "45aaa542e4acf24e7bc1",
+    "url": "/app-docs/static/js/2.c61be2f6.chunk.js"
+  },
+  {
+    "revision": "fc3e26c63bfcad6ba0872ffb59342e0b",
+    "url": "/app-docs/static/js/2.c61be2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858a7c063c8986853998",
+    "url": "/app-docs/static/js/main.720af4e5.chunk.js"
+  },
+  {
+    "revision": "9ba95b79c2756bef1bf0",
+    "url": "/app-docs/static/js/runtime-main.acd66041.js"
+  },
+  {
+    "revision": "ddf98d25935110f3ea1e83d1154f073c",
+    "url": "/app-docs/static/media/panther-icon.ddf98d25.png"
+  }
+]);
