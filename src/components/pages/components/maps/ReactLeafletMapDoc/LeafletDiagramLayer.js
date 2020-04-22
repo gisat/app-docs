6ee07d7494da9ebe6033e808e1ebdea433bd9eff
vlach1989 <vlach1989@gmail.com>
2020-04-22 08:09:49 +0200

    LeafletDiagramLayer update

--- src/components/pages/components/maps/ReactLeafletMapDoc/LeafletDiagramLayer.js
+++ src/components/pages/components/maps/ReactLeafletMapDoc/LeafletDiagramLayer.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import _ from "lodash";
 import Page, {
-    DocsToDo,
-    DocsToDoInline,
-    ImplementationToDo,
-    InlineCodeHighlighter,
-    LightDarkBlock,
     SyntaxHighlighter
 } from "../../../../Page";
 import {MapControls, PresentationMap, ReactLeafletMap} from "@gisatcz/ptr-maps";
-import ComponentPropsTable, {Prop} from "../../../../ComponentPropsTable/ComponentPropsTable";
 import cz_gadm from "../../../../mockData/map/czGadm1WithStyles/geometries.json";
-import {utils} from "@gisatcz/ptr-utils";
 import nuts_2 from "../../../../mockData/map/nuts_2.json";
 import {HoverHandler} from "@gisatcz/ptr-core";
+import {Link} from "@gisatcz/ptr-state";
 
 const europeView = {
     center: {lat: 49.8, lon: 12},
@@ -71,7 +65,8 @@ const diagramChoroplethStyle = {rules: [{
             outlineWidth: 1,
             outlineColor: "#666",
             diagramShape: "circle",
-            diagramFillOpacity: 1
+            diagramFill: "#14a8b4",
+            diagramOutlineColor: "#0d393d"
         }, {
             attributeKey: "positive_attr",
             attributeScale: {
@@ -157,7 +152,65 @@ class LeafletDiagramLayer extends React.PureComponent {
     render() {
         return (
             <Page title="Leaflet diagram layer">
+                <p>Use diagram to visualize absolute value for area. See the examples bellow how diagrams can be drawn on the top of choropleth map.</p>
+                <p>DiagramLayer component extends <Link to="./vectorLayer">VectorLayer</Link> component and is always used inside ReactLeafletMap component. The data are passed via layers prop (see <Link to="/components/maps/map">Map</Link> documentation), where each layer is represented by Vector layer data type. For general information about Vector layer system data type, see <Link to="/architecture/systemDataTypes/layers#vector">Layers</Link> section.</p>
+                <p>In case of Diagram layer, following options are different from Vector layer data type definition:</p>
+                <SyntaxHighlighter language="javascript">
+                    {
+                        `{
+    // ...
+	type: "diagram"
+}`
+                    }
+                </SyntaxHighlighter>
                 <h2>Basic diagrams</h2>
+                <SyntaxHighlighter language="jsx">{`<PresentationMap
+    mapKey={"czechia"}
+    mapComponent={ReactLeafletMap}
+    view={{
+        center: {lat: 50, lon: 15},
+        boxRange: 2000000
+    }}
+    backgroundLayer={{
+        key: 'background-osm',
+        type: 'wmts',
+        options: {
+            url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        }
+    }}
+    layers={[{
+        key: "polygons",
+        type: "diagram",
+        options: {
+            features: cz_gadm.features,
+            style: {rules: [{
+                styles: [{
+                    fill: "#cccccc",
+                    diagramShape: "circle",
+                    diagramFillOpacity: 0.85
+                }, {
+                    attributeKey: "e575b4d4-7c7a-4658-bb9a-a9b61fcc2587",
+                    attributeScale: {
+                        diagramSize: {
+                            "inputInterval": [0,10],
+                            "outputInterval": [10000, 30000]
+                        }
+                    }
+                }]
+            }]},
+            fidColumnName: "GID_1",
+            selected: {
+                "testSelection": {
+                    keys: ["CZE.12_1"]
+                }
+            }
+        }
+    }]}
+    onLayerClick={this.onLayerClick}
+>
+    <MapControls zoomOnly levelsBased/>
+</PresentationMap>`}
+                </SyntaxHighlighter>
                 <div style={{height: 500, marginBottom: 10}}>
                     <HoverHandler
                         popupContentComponent={
@@ -181,6 +234,68 @@ class LeafletDiagramLayer extends React.PureComponent {
 
 
                 <h2>Basic diagrams with choropleth</h2>
+                <SyntaxHighlighter language="jsx">{`<PresentationMap
+    //...
+    layers={[{
+        key: "countries",
+        type: "diagram",
+        options: {
+            features: nuts_2.features,
+            fidColumnName: "id",
+            selected: {
+                "testSelection": {
+                    keys: ["PL41"]
+                }
+            },
+            style: {rules: [{
+                styles: [{
+                    outlineWidth: 1,
+                    outlineColor: "#666",
+                    diagramShape: "circle",
+                    diagramFill: "#14a8b4",
+                    diagramOutlineColor: "#0d393d"
+                }, {
+                    attributeKey: "positive_attr",
+                    attributeScale: {
+                        diagramSize: {
+                            "inputInterval": [0,10],
+                            "outputInterval": [2000, 20000]
+                        }
+                    }
+                }, {
+                    attributeKey: "diverging_attr",
+                    attributeClasses: [
+                        {
+                            interval: [-5,-3],
+                            intervalBounds: [true, false],
+                            fill: "#d7191c"
+                        },
+                        {
+                            interval: [-3,-1],
+                            intervalBounds: [true, false],
+                            fill: "#fdae61"
+                        },{
+                            interval: [-1,1],
+                            intervalBounds: [true, false],
+                            fill: "#ffffbf"
+                        },{
+                            interval: [1,3],
+                            intervalBounds: [true, false],
+                            fill: "#a6d96a"
+                        },{
+                            interval: [3,5],
+                            intervalBounds: [true, false],
+                            fill: "#1a9641"
+                        }
+                    ]
+                }]
+            }]} 
+        }
+    }]}
+>
+    <MapControls zoomOnly levelsBased/>
+</PresentationMap>`}
+                </SyntaxHighlighter>
                 <div style={{height: 500, marginBottom: 10}}>
                     <HoverHandler
                         popupContentComponent={
