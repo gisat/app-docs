b5c6c77204a9ea6a7bf7a22231ef458e47500904
vlach1989 <vlach1989@gmail.com>
2020-05-07 09:38:15 +0200

    LeafletLargeDataLayer - updates & remote static data repository

--- src/components/pages/components/maps/ReactLeafletMapDoc/LeafletLargeDataLayer.js
+++ src/components/pages/components/maps/ReactLeafletMapDoc/LeafletLargeDataLayer.js
@@ -9,19 +9,16 @@ import Page, {
     SyntaxHighlighter
 } from "../../../../Page";
 
-import cz_10000 from '../../../../mockData/largeData/cz_10000.json';
-import pointStyle from "../../../../mockData/map/largePointData/style-simple-point.json";
-import pointData from "../../../../mockData/map/largePointData/geometries.json";
 import {ReactLeafletMap, PresentationMap} from "@gisatcz/ptr-maps";
 import {HoverHandler} from "@gisatcz/ptr-core";
-import {Button, Buttons} from "@gisatcz/ptr-atoms";
-
-const cz_10000_features = cz_10000.features;
+import {Button, Buttons, ButtonSwitchOption, ButtonGroup} from "@gisatcz/ptr-atoms";
+import utils from "../../../../../utils";
+import config from "../../../../../config";
 
 // *** VIEWS ***
 const view = {
     center: {lat: 50.05, lon: 15.15},
-    boxRange: 100000
+    boxRange: 30000
 };
 
 const backgroundLayer = {
@@ -33,7 +30,7 @@ const backgroundLayer = {
 };
 
 // *** POINTS ***
-const cz_10000_style = {
+const cz_large_data_style = {
     "rules":[
         {
             "styles": [
@@ -76,49 +73,69 @@ const cz_10000_style = {
                             "shape": "diamond"
                         }
                     ]
+                }, {
+                    "attributeKey": "a3",
+                    "attributeScale": {
+                        "size": {
+                            "inputInterval": [10,10000],
+                            "outputInterval": [5,15]
+                        }
+                    }
                 }
             ]
         }
     ]
 }
 
-const cz_10000_layer = {
-    key: "cz_10000_layer",
+const defaultMaxBoxRange = 50000;
+
+const cz_large_data_layer = {
+    key: "cz_large_data_layer",
     type: "vector-large",
     options: {
         features: [],
         selected: {
           testSelection: {}
         },
-        style: cz_10000_style,
+        style: cz_large_data_style,
         pointAsMarker: true,
         fidColumnName: "gid",
-        maxBoxRange: 200000
+        boxRangeRange: [null, defaultMaxBoxRange]
     }
 };
 
-const cz_10000_layers = [cz_10000_layer];
+const cz_large_data_layers = [cz_large_data_layer];
 
 class LeafletLargeDataLayer extends React.PureComponent {
     constructor(props) {
         super(props);
-        this.state = {cz_10000_layers};
+        this.state = {
+            cz_large_data_layers,
+            maxBoxRange: defaultMaxBoxRange,
+            currentBoxRange: view.boxRange
+        };
 
         this.onLayerClick = this.onLayerClick.bind(this);
-        this.addAll = this.addAll.bind(this);
         this.removeAll = this.removeAll.bind(this);
+        this.onViewChange = this.onViewChange.bind(this);
+    }
+
+    onViewChange(view) {
+        this.setState({
+            currentBoxRange: view.boxRange
+        });
     }
 
     onLayerClick(map, layer, features) {
-        if (layer === 'cz_10000_layer') {
+        if (layer === 'cz_large_data_layer') {
             let updatedLayers = [{
-                ...this.state.cz_10000_layers[0],
+                ...this.state.cz_large_data_layers[0],
                 options: {
-                    ...this.state.cz_10000_layers[0].options,
+                    ...this.state.cz_large_data_layers[0].options,
                     selected: {
-                        ...this.state.cz_10000_layers[0].options.selected,
+                        ...this.state.cz_large_data_layers[0].options.selected,
                         testSelection: {
-                            ...this.state.cz_10000_layers[0].options.selected.testSelection,
+                            ...this.state.cz_large_data_layers[0].options.selected.testSelection,
                             keys: features
                         }
                     }
@@ -126,59 +143,84 @@ class LeafletLargeDataLayer extends React.PureComponent {
             }];
 
             this.setState({
-                cz_10000_layers: updatedLayers
+                cz_large_data_layers: updatedLayers
             })
         }
     }
 
-    addAll() {
-        this.populateLayer('cz_10000_layer', cz_10000_features);
-    }
-
-    addAllPerPartes(amount, step, speed) {
-        if (!step) {
-            step = amount;
+    addData(limit) {
+        let promises = [];
+        for (let i=1; i<=limit; i++) {
+            let url = `${config.mockDataRepositoryUrl}pvlach/largeData/cz_200000/${i}.json`;
+            promises.push(utils.request(url, "GET"));
         }
 
-        let features = cz_10000_features.slice(0, amount - 1);
-        this.populateLayer('cz_10000_layer', features);
-
-        if (amount <= cz_10000_features.length) {
-            const self = this;
-            setTimeout(() => {
-                self.addAllPerPartes(amount + step, step, speed);
-            }, speed);
+        if (promises.length) {
+            this.setState({loading: ["Loading started..."]});
+            Promise.all(promises).then(data => {
+               if (data.length) {
+                   this.setState({loading: [...this.state.loading, "Data loaded"]});
+                   const features = _.flatten(data.map(item => item.features));
+                   this.populateLayer('cz_large_data_layer', features);
+               }
+            }).catch(err => new Error(err));
         }
     }
 
     removeAll() {
-        this.populateLayer('cz_10000_layer', []);
+        this.populateLayer('cz_large_data_layer', []);
     }
 
     populateLayer(layer, features) {
-        if (layer === 'cz_10000_layer') {
+        if (layer === 'cz_large_data_layer') {
             let updatedLayers = [{
-                ...cz_10000_layer,
+                ...this.state.cz_large_data_layers[0],
                 options: {
-                    ...cz_10000_layer.options,
+                    ...this.state.cz_large_data_layers[0].options,
                     features
                 }
             }];
 
             this.setState({
-                cz_10000_layers: updatedLayers
-            })
+                cz_large_data_layers: updatedLayers,
+                loading: features.length ? [...this.state.loading, "Add data to map"] : ["Remove data"]
+            });
         }
+    }
+
+    onMaxRangeChange(maxBoxRange) {
+        let updatedLayers = [{
+            ...this.state.cz_large_data_layers[0],
+            options: {
+                ...this.state.cz_large_data_layers[0].options,
+                boxRangeRange: [null, maxBoxRange]
+            }
+        }];
+
+        this.setState({
+            maxBoxRange,
+            cz_large_data_layers: updatedLayers
+        })
     }
 
     render() {
         return (
             <Page title="Leaflet Large Data Layer">
-                <div style={{height: 500, marginBottom: 10}}>
+                <div>
+                    <div>Current boxRange: {Math.round(this.state.currentBoxRange/1000)} km</div>
+                    <div>Maximum boxRange to draw features:</div>
+                    <ButtonGroup>
+                        <ButtonSwitchOption primary={this.state.maxBoxRange === 20000} onClick={this.onMaxRangeChange.bind(this, 20000)}>20 km</ButtonSwitchOption>
+                        <ButtonSwitchOption primary={this.state.maxBoxRange === 50000} onClick={this.onMaxRangeChange.bind(this, 50000)}>50 km</ButtonSwitchOption>
+                        <ButtonSwitchOption primary={this.state.maxBoxRange === 100000} onClick={this.onMaxRangeChange.bind(this, 100000)}>100 km</ButtonSwitchOption>
+                    </ButtonGroup>
+                </div>
+                <div style={{height: 500, marginBottom: 10, marginTop: 10}}>
                     <HoverHandler
                         popupContentComponent={
                             (props) => <>
                                 Value for color: <b>{props.data["a1"]}</b><br/>
+                                Value for size: <b>{props.data["a3"]}</b><br/>
                                 Value for shape: <b>{props.data["a2"]}</b>
                             </>
                         }
@@ -188,16 +230,18 @@ class LeafletLargeDataLayer extends React.PureComponent {
                             mapKey='large-data'
                             view={view}
                             backgroundLayer={backgroundLayer}
-                            layers={this.state.cz_10000_layers}
+                            layers={this.state.cz_large_data_layers}
                             onLayerClick={this.onLayerClick}
+                            onViewChange={this.onViewChange}
                         />
                     </HoverHandler>
                 </div>
                 <Buttons>
-                    <Button onClick={this.addAll}>Add all data at once</Button>
-                    <Button onClick={this.addAllPerPartes.bind(this, 1000, 1000, 100)}>Add all data per partes</Button>
+                    <Button onClick={this.addData.bind(this,1)}>Add 20 000 points</Button>
+                    <Button onClick={this.addData.bind(this,10)}>Add 200 000 points</Button>
                     <Button onClick={this.removeAll}>Remove all data</Button>
                 </Buttons>
+                {this.state.loading && this.state.loading.map((step, i) => <div key={i}>{step}</div>)}
             </Page>
         );
     }
