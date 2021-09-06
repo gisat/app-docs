c813d6850ff0ffd2d19909db5a64e9967d0ed6df
PavelVlach <vlach1989@gmail.com>
2021-09-06 18:05:01 +0200

    LeafletCogLayer - basic doc

--- src/components/pages/components/maps/ReactLeafletMapDoc/LeafletCogLayer.js
+++ src/components/pages/components/maps/ReactLeafletMapDoc/LeafletCogLayer.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Page from '../../../../Page';
+import Page, {SyntaxHighlighter} from '../../../../Page';
 import {ReactLeafletMap} from '@gisatcz/ptr-maps';
 
 const view = {
-	center: {lat: -33.8, lon: -59.733},
+	center: {lat: -33.9, lon: -59.733},
 	boxRange: 80000,
 };
 
-const backgroundLayer = {
+const osmLayer = {
 	key: 'background-osm',
 	type: 'wmts',
 	options: {
@@ -15,24 +15,119 @@ const backgroundLayer = {
 	},
 };
 
-const layer = {
+const croplandClassificationLayer = {
 	key: 'cog-layer',
 	type: 'cog',
+	opacity: 0.7,
 	options: {
 		url: 'https://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/example/21HTC/2020_21HTC_annualcropland_classification_WorldCerealPixelLSTM.tif',
+		style: {
+			rules: [
+				{
+					styles: [
+						{
+							color: '#000000',
+						},
+						{
+							bandIndex: 0,
+							values: {
+								0: {
+									color: null,
+								},
+							},
+						},
+					],
+				},
+			],
+		},
+	},
+};
+
+const croplandConfidenceLayer = {
+	key: 'cog-layer-confidence',
+	type: 'cog',
+	options: {
+		url: 'https://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/example/21HTC/2020_21HTC_annualcropland_confidence_WorldCerealPixelLSTM.tif',
+		style: {
+			rules: [
+				{
+					styles: [
+						{
+							color: '#000000',
+						},
+						{
+							bandIndex: 0,
+							values: {
+								0: {
+									color: null,
+								},
+							},
+						},
+					],
+				},
+			],
+		},
+	},
+};
+
+const croplandMetaLayer = {
+	key: 'cog-layer-meta',
+	type: 'cog',
+	options: {
+		url: 'https://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/example/21HTC/2020_21HTC_annualcropland_metafeatures_WorldCerealPixelLSTM.tif',
 	},
 };
 
 class LeafletCogLayer extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			basicExampleLayers: [croplandClassificationLayer],
+		};
+	}
+
 	render() {
 		return (
 			<Page title="Leaflet COG layer">
-				<div style={{height: 600, marginBottom: 10}}>
+				<SyntaxHighlighter language="jsx">
+					{`<ReactLeafletMap
+	mapKey="react-leaflet-map-cog-3"
+	view={view}
+	backgroundLayer={{
+		key: 'background-osm',
+		type: 'wmts',
+		options: {
+			url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+		}
+	}}
+	layers={[{
+		key: 'cog-layer',
+		type: 'cog',
+		opacity: 0.7,
+		options: {
+			url: 'https://gisat-gis.eu-central-1.linodeobjects.com/worldcereal/example/21HTC/2020_21HTC_annualcropland_classification_WorldCerealPixelLSTM.tif',
+			style: {
+				rules: [{
+					styles: [{
+						color: "#000000"
+					},{
+						bandIndex: 0,
+						values: {
+							0: {color: null}
+						}
+					}]
+				}]
+			}
+		}
+	}]}
+/>`}
+				</SyntaxHighlighter>
+				<div style={{height: 500, marginBottom: 10}}>
 					<ReactLeafletMap
-						mapKey="react-leaflet-map-cog"
+						mapKey="react-leaflet-map-cog-3"
 						view={view}
-						backgroundLayer={backgroundLayer}
-						layers={[layer]}
+						backgroundLayer={osmLayer}
+						layers={this.state.basicExampleLayers}
 					/>
 				</div>
 			</Page>
