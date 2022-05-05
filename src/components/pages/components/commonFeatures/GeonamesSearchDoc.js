c91c1ec5a9d41579ff114da7b652ac1741feaca3
PavelVlach <vlach1989@gmail.com>
2022-05-05 17:23:21 +0200

    GeonamesSearchDoc: zoom to place

--- src/components/pages/components/commonFeatures/GeonamesSearchDoc.js
+++ src/components/pages/components/commonFeatures/GeonamesSearchDoc.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ReactLeafletMap} from '@gisatcz/ptr-maps';
 import {GeonamesSearch} from '@gisatcz/ptr-components';
 import ComponentPropsTable from '../../../ComponentPropsTable/ComponentPropsTable';
 import Page from '../../../Page';
 
+const defaultView = {
+	center: {lat: 50, lon: 15},
+	boxRange: 400000,
+};
+
+const backgroundLayer = {
+	key: 'background-osm',
+	type: 'wmts',
+	options: {
+		url: 'https://{s}.tile.osm.org/{z}/{x}/{y}.png',
+	},
+};
+
 const GeonamesSearchDoc = () => {
+	const [view, setView] = useState(defaultView);
+
+	const onPlaceSelect = place => {
+		setView(place.pantherView);
+	};
+
 	return (
 		<Page title="GeonamesSearch">
 			<h2 id="props">Props</h2>
@@ -32,8 +52,13 @@ const GeonamesSearchDoc = () => {
 			/>
 
 			<h2 id="usage">Usage</h2>
-			<div>
-				<GeonamesSearch />
+			<div style={{position: 'relative'}}>
+				<div style={{position: 'absolute', top: 10, left: 10, zIndex: 99}}>
+					<GeonamesSearch onPlaceSelect={onPlaceSelect} />
+				</div>
+				<div style={{height: 600}}>
+					<ReactLeafletMap view={view} backgroundLayer={backgroundLayer} />
+				</div>
 			</div>
 		</Page>
 	);
