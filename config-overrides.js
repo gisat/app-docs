c6302b7eab5aab8887c0a3c03a839db86c7bf025
Vojtěch Dubrovský <v.dubr@hotmail.com>
2022-05-16 08:41:15 +0200

    Update maps package

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,12 @@ module.exports = function override(config, env) {
 	// TODO comment when using package from npm
 	config.resolve = {
 		alias: {
+			// '@gisatcz/ptr-state': '/Users/vojtadubrovsky/_WORK/GISAT/git/ptr-state',
+			// '@gisatcz/ptr-maps': '/Users/vojtadubrovsky/_WORK/GISAT/git/ptr-maps',
+			// react:
+			// 	'/Users/vojtadubrovsky/_WORK/GISAT/git/ptr-maps/node_modules/react',
+			// 'react-resize-detector': '/Users/vojtadubrovsky/_WORK/GISAT/git/ptr-timeline/node_modules/react-resize-detector',
+			// 'react-dom': '/Users/vojtadubrovsky/_WORK/GISAT/git/ptr-state/node_modules/react-dom',
 			// "@gisatcz/ptr-charts": "C:/Users/pvlach/DATA/ptr-charts",
 			// react: 'C:/Users/PavelVlach/WebstormProjects/ptr-maps/node_modules/react',
 			// '@gisatcz/ptr-maps': 'C:/Users/PavelVlach/WebstormProjects/ptr-maps',
